8a8f13309edf7ad9e5da6a1bf8540533bddc77e8
Andrew Joslin <andrewtjoslin@gmail.com>
2016-03-19 20:45:10 -0700

    Use indentedSyntax when parsing .sass files, allow options to be passed in
    Signed-off-by: Yoshua Wuyts <i@yoshuawuyts.com>

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const sass = require('node-sass')
+const extend = require('xtend')
 
 module.exports = sheetifySass
 
 // SASS plugin for Sheetify
 // (str, str, obj, fn) -> null
 function sheetifySass (filename, source, options, done) {
-  const sassOpts = {
+  const sassOpts = extend({
     data: source,
-    file: filename
-  }
+    file: filename,
+    indentedSyntax: /\.sass$/i.test(filename)
+  }, options)
+
   sass.render(sassOpts, function (err, res) {
     if (err) return done(err)
     done(null, String(res.css))
