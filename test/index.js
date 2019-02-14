83753dc29370fc9e991b2dd9a6ec01daddb9c219
Renée Kooi <renee@kooi.me>
2019-02-14 10:28:04 +0100

    Add includedFiles test

--- test/index.js
+++ test/index.js
@@ -8,19 +8,30 @@ test('sheetify-sass', function (t) {
   t.test('should transform sass and scss', function (t) {
     const expected = fs.readFileSync(path.join(__dirname, 'expected.css'))
 
-    t.plan(2)
+    t.plan(4)
+
+    const files = new Set()
     browserify(path.join(__dirname, 'source.js'))
+      .on('transform', function (transform) {
+        transform.on('file', function (filename) {
+          files.add(filename)
+        })
+      })
       .transform('sheetify', {
         transform: [ './' ]
       })
       .plugin('css-extract', {
         out: createWs
-      }).bundle()
+      })
+      .bundle()
 
     function createWs () {
       return bl(function (err, buf) {
         t.ifError(err, 'no error')
         t.equal(String(buf).trim(), String(expected).trim())
+
+        t.ok(files.has(path.join(__dirname, 'import.scss')))
+        t.ok(files.has(path.join(__dirname, 'indent-import.sass')))
       })
     }
   })
